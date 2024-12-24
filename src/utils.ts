39f1813f10998ab9204fe8e0ddf86b2e531bf559
Jeroen Peelaerts <159421226+jplrts@users.noreply.github.com>
2024-12-24 17:13:46 +0100

    Update utils.ts
    Do not error on status event

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,9 @@ import * as github from '@actions/github'
 export async function validateEventType(): Promise<void> {
   if (
     github.context.eventName !== 'pull_request' &&
-    github.context.eventName !== 'push'
+    github.context.eventName !== 'push' && 
+    github.context.eventName !== 'status'
   ) {
-    throw new Error('Error, action only works for pull_request or push events!')
+    throw new Error('Error, action only works for pull_request, push or status events!')
   }
 }
